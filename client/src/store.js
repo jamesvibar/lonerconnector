6e5378104f639ddc394046db81e72c5f332759bd
James Raphael Vibar <vibarjamess@gmail.com>
2018-12-07 00:07:38 +0800

    fix store.js

--- client/src/store.js
+++ client/src/store.js
@@ -1,4 +1,5 @@
-import { createStore, applyMiddleware, compose } from "redux";
+// import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
