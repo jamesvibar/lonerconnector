22a87d575783cec8e4e36549816369f2913f7264
James Raphael Vibar <vibarjamess@gmail.com>
2018-12-08 20:10:44 +0800

    Add dotenv ENVIRONMENT VARIABLES

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
+require("dotenv").config();
 const app = express();
 const db = require("./config/keys").mongoURI;
 
@@ -28,25 +29,17 @@ app.use(passport.initialize());
 // Passport Config -
 require("./config/passport")(passport);
 
-app.use(express.static(`${__dirname}/client/build`));
-app.get("*", (req, res) =>
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-);
-
-// Server static assets if in production
-// if (process.env.NODE_ENV === "production") {
-//   // Set static folder
-//   app.use(express.static("client/build"));
-
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-//   );
-// }
-
 // Routes ===============================
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(`${__dirname}/client/build`));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on ${port}`));
